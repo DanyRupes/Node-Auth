cc4d4c8bbbf5625f56e9e7450b0542f4e8b32e3b
danyrupes <danyrupes26@gmail.com>
2019-08-09 00:17:41 +0530

    scroll, width sidebar fix

---
+++ client/build/precache-manifest.c895811af55f8aad58453cb1e23cf2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590c99ee295f4d9cc932328167a41b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21cef116735dae21c741",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "90a6a4ec4aaf5cfe7c70",
+    "url": "/static/css/main.314388e5.chunk.css"
+  },
+  {
+    "revision": "21cef116735dae21c741",
+    "url": "/static/js/2.3959de3f.chunk.js"
+  },
+  {
+    "revision": "90a6a4ec4aaf5cfe7c70",
+    "url": "/static/js/main.87598d8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
